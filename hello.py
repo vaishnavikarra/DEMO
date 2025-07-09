59f614eb3c02a9acbc2ff0f4963312a3445d8100
vaishnavikarra <vaishnavikarra869@gmail.com>
2025-07-09 10:33:50 +0530

    my project

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def hello():
-    return ("Hello Welcome")
+    return ("Hello Welcome to gnits
+")
 if __name__=="__main__":
     app.run(debug=True)
